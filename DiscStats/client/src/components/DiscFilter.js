dac2f2c19fc56ec46083912ff3e51ea359ef8bdd
blaker814 <rab0196@yahoo.com>
2021-02-05 09:38:33 -0600

    Added a search filter for discs

---
+++ DiscStats/client/src/components/DiscFilter.js
@@ -0,0 +1,23 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form, Input } from "reactstrap"
+import useWindowDimensions from "../utils/getWindowDimensions"
+
+export const DiscFilter = ({ searchTerms, setSearchTerms }) => {
+    const { width } = useWindowDimensions();
+
+    return (
+        <Form className={width > 576 ? (width < 768 ? "input-group ml-auto mr-3 my-4 w-50" : "input-group mt-4 ml-auto mr-3 w-25") : "input-group my-4 mx-3"}
+            onSubmit={e => e.preventDefault()}
+        >
+            <Input value={searchTerms}
+                className="border-right-0"
+                placeholder="Filter discs"
+                onChange={e => setSearchTerms(e.target.value)}
+            />
+            <span className="input-group-append">
+                <div className="input-group-text bg-transparent"><FontAwesomeIcon icon={faSearch} /></div>
+            </span>
+        </Form>
+    )
+}
--- DiscStats/client/src/pages/DiscManager.js
+++ DiscStats/client/src/pages/DiscManager.js
@@ -4,9 +4,12 @@ import { DiscCard } from "../components/DiscCard";
 import { Button } from "reactstrap";
 import useWindowDimensions from "../utils/getWindowDimensions";
 import { useHistory } from "react-router-dom";
+import { DiscFilter } from "../components/DiscFilter";
 
 export const DiscManager = () => {
     const [discs, setDiscs] = useState([]);
+    const [searchTerms, setSearchTerms] = useState("");
+    const [filtered, setFiltered] = useState([]);
     const { getToken } = useContext(UserContext);
     const { width } = useWindowDimensions();
     const history = useHistory();
@@ -28,24 +31,42 @@ export const DiscManager = () => {
         );
     }, []);
 
+    useEffect(() => {
+        if (searchTerms !== "") {
+            // If the search field is not blank, display matching friends
+            const subset = discs.filter(disc => {
+                return disc.name.toLowerCase().includes(searchTerms.toLowerCase())
+            })
+            setFiltered(subset)
+        } else {
+            // If the search field is blank, display all user friends
+            setFiltered(discs)
+        }
+    }, [searchTerms, discs])
+
     return (
-        <div className="container mt-5">
-            <h2>Discs</h2>
-            <Button type="button"
-                block={width < 992}
-                onClick={() => history.push("/discs/add")}
-                color="danger">Add Disc</Button>
-            <div className="row justify-content-center">
-                {
-                    discs.map(disc => {
-                        return (
-                            <div key={disc.id} className="m-4">
-                                <DiscCard disc={disc} />
-                            </div>
-                        )
-                    })
-                }
+        <>
+            <div className="row">
+                <DiscFilter searchTerms={searchTerms} setSearchTerms={setSearchTerms} />
             </div>
-        </div>
+            <div className="container">
+                <h2>Discs</h2>
+                <Button type="button"
+                    block={width < 992}
+                    onClick={() => history.push("/discs/add")}
+                    color="danger">Add Disc</Button>
+                <div className="row justify-content-center">
+                    {
+                        filtered.map(disc => {
+                            return (
+                                <div key={disc.id} className="m-4">
+                                    <DiscCard disc={disc} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
     );
 };
